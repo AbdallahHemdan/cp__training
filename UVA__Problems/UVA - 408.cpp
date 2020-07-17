528b67c6b32a223bbbe3978d34e4c1b8e2e61d88
Abdallah Hemdan <abdallah.ahmed.hemdan@gmail.com>
2020-07-18 01:38:40 +0200

    Create UVA - 408.cpp

---
+++ UVA__Problems/UVA - 408.cpp
@@ -0,0 +1,53 @@
+// Author : Abdallah Hemdan
+// For Hassanosama, ICPC next year ISA.
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+#include <bits/stdc++.h>
+#define vll vector<ll>
+#define vi vector<int>
+#define sz size()
+#define pii pair<int,int>
+#define IO ios_base::sync_with_stdio(0),ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define all(v) ((v).begin()), ((v).end())
+#define rall(v) ((v).rbegin()), ((v).rend())
+
+using namespace std;
+using ll = long long int;
+using ld = long double;
+
+int MOD = 1e9 + 7;
+const double EPS = 1e-9;
+const int oo = INT_MAX;
+const ll OO = 1e18;
+
+const int dx[] = { 1, -1, 0, 0 };
+const int dy[] = { 0, 0, 1, -1 };
+
+ll GCD(ll a, ll b) { return ((!b) ? a : GCD(b, a%b)); }
+ll LCM(ll a, ll b) { return a / (GCD(a, b))*b; }
+
+int main() {
+	IO;
+#ifndef ONLINE_JUDGE
+	freopen("In.txt", "r", stdin);
+#else
+	// freopen("input.in","r",stdin);
+	// freopen("output.out","w",stdout);
+#endif
+	ll s, mod;
+	set<ll> st;
+	while (cin >> s >> mod) {
+		st.clear();
+		ll cur = 0;
+		for (ll i = 0; i < mod; i++) {
+			cur += s;
+			cur %= mod;
+			if (st.count(cur)) break;
+			st.insert(cur);
+		}
+		cout << setw(10) << s << setw(10) << mod;
+		(st.sz != mod) ? cout << "    Bad Choice\n\n" : cout << "    Good Choice\n\n";
+	}
+}
+
