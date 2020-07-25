04295c0d6c22668db183aadbc1a540d920631682
Abdallah Hemdan <abdallah.ahmed.hemdan@gmail.com>
2020-07-25 05:53:38 +0200

    Create E - Epic Professor.cpp

---
+++ GYM__Contests/3__Star/2015 ACM Amman Collegiate Programming Contest/E - Epic Professor.cpp
@@ -0,0 +1,57 @@
+// Author : Abdallah Hemdan
+// For Hassanosama, ICPC next year ISA.
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+#include <bits/stdc++.h>
+#define vll vector<ll>
+#define vi vector<int>
+#define sz size()
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define IO ios_base::sync_with_stdio(0),ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define all(v) ((v).begin()), ((v).end())
+#define rall(v) ((v).rbegin()), ((v).rend())
+
+using namespace std;
+using ll = long long int;
+using ld = long double;
+
+int MOD = 1e9 + 7;
+const double EPS = 1e-9;
+const int oo = INT_MAX;
+const ll OO = 1e18;
+
+const int dx[] = { 1, -1, 0, 0 };
+const int dy[] = { 0, 0, 1, -1 };
+
+ll GCD(ll a, ll b) { return ((!b) ? a : GCD(b, a%b)); }
+ll LCM(ll a, ll b) { return a / (GCD(a, b))*b; }
+
+
+int main() {
+	IO;
+#ifndef ONLINE_JUDGE
+	freopen("In.txt", "r", stdin);
+#else
+	// freopen("input.in","r",stdin);
+	// freopen("output.out","w",stdout);
+#endif
+	ll t; cin >> t;
+	while (t--) {
+		ll n; cin >> n;
+		ll mx = LLONG_MIN;
+		vll a(n);
+		for (auto& o : a) {
+			cin >> o;
+			mx = max({ o, mx });
+		}
+		ll bonus = 100 - mx;
+		for (auto& i : a) i += bonus;
+		ll ret = 0;
+		for (auto i : a) {
+			ret += (i >= 50);
+		}
+		cout << ret << endl;
+	}
+}
