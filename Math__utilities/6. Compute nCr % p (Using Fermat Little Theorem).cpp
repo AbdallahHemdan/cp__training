78dbbbedad8005412d935baaf189e8d4daa549a5
Abdallah Hemdan <abdallah.ahmed.hemdan@gmail.com>
2020-10-11 04:32:04 +0200

    Update 6. Compute nCr % p (Using Fermat Little Theorem).cpp

--- Math__utilities/6. Compute nCr % p (Using Fermat Little Theorem).cpp
+++ Math__utilities/6. Compute nCr % p (Using Fermat Little Theorem).cpp
@@ -1,28 +1,26 @@
 const int MOD = 1e9 + 7;
 const int N = 1e5;
 ll fact[N + 5];
-ll factpow[N + 5];
-ll sqr(ll a) {
-	return (a * a) % MOD;
-}
-ll modpow(ll a, ll b){
-	if(b == 0) return 1;
-	if(b == 1) return a % MOD;
-	ll tmp = sqr(modpow(a , b >> 1));
-	if(b&1) tmp = (tmp * a) % MOD;
-	return tmp;
+ll factInv[N + 5];
+ll sqr(ll a) { return (a * a) % MOD; }
+ll modpow(ll a, ll b) {
+    if (b == 0) return 1;
+    if (b == 1) return a % MOD;
+    ll tmp = sqr(modpow(a, b >> 1));
+    if (b & 1) tmp = (tmp * a) % MOD;
+    return tmp;
 }
 
-void init(){
-	fact[0] = fact[1] = 1;
-	for(int i = 2;i <= N; ++i){
-		fact[i] = (fact[i-1] * i) % MOD;
-	}
-	for(int i = 0;i <= N; ++i){
-		factpow[i] = modpow(fact[i], MOD - 2);
-	}
+void init() {
+    fact[0] = fact[1] = 1;
+    for (int i = 2; i <= N; ++i) {
+        fact[i] = (fact[i - 1] * i) % MOD;
+    }
+    for (int i = 0; i <= N; ++i) {
+        factInv[i] = modpow(fact[i], MOD - 2);
+    }
 }
 
-ll C(int a, int b){
-	return (((fact[a] * factpow[b]) % MOD) * factpow[a - b]) % MOD;
+ll nCr(int a, int b) {
+    return (((fact[a] * factInv[b]) % MOD) * factInv[a - b]) % MOD;
 }
