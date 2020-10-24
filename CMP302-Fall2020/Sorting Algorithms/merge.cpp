bf25c6bb4d3d457f742b79c501a0c6f631cc752d
Abdallah Hemdan <abdallah.ahmed.hemdan@gmail.com>
2020-10-24 23:27:17 +0200

    Create merge.cpp

---
+++ CMP302-Fall2020/Sorting Algorithms/merge.cpp
@@ -0,0 +1,65 @@
+void merge(vector<int> &a, int l, int m, int r) {
+    int lftSz = m - l + 1;
+    int rgtSz = r - m;
+
+    vector<int> L(lftSz);
+    vector<int> R(rgtSz);
+
+    for (int i = 0; i < lftSz; i++) {
+        L[i] = a[l + i];
+    }
+    for (int j = 0; j < rgtSz; j++) {
+        R[j] = a[m + 1 + j];
+    }
+
+    int k = l;
+    int lft = 0;
+    int rgt = 0;
+
+    while (lft < lftSz && rgt < rgtSz) {
+        if (L[lft] <= R[rgt]) {
+            a[k] = L[lft++];
+        } else {
+            a[k] = R[rgt++];
+        }
+        k++;
+    }
+
+    while (lft < lftSz) {
+        a[k] = L[lft++];
+        k++;
+    }
+    while (rgt < rgtSz) {
+        a[k] = R[rgt++];
+        k++;
+    }
+}
+
+void mergeSort(vector<int> &a, int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(a, l, m);
+        mergeSort(a, m + 1, r);
+
+        merge(a, l, m, r);
+    }
+}
+
+void MergeSort(vector<int> &a) {
+    int n = a.size() - 1;
+    mergeSort(a, 0, n);
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int &i :a) cin >> i;
+
+    MergeSort(a);
+
+    for (int i = 0; i < n; ++i) {
+        cout << a[i] << " \n"[i == n];
+    }
+}
