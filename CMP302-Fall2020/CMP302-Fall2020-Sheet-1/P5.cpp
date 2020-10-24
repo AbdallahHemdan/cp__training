d9ed5898bf01898e2c71dd92f80bf6951aea94d1
Abdallah Hemdan <abdallah.ahmed.hemdan@gmail.com>
2020-10-24 17:29:41 +0200

    Create P5.cpp

---
+++ CMP302-Fall2020/CMP302-Fall2020-Sheet-1/P5.cpp
@@ -0,0 +1,74 @@
+// Author : Abdallah Hemdan
+// For Hassanosama, ICPC next year ISA.
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+#include <bits/stdc++.h>
+#define vll vector<ll>
+#define vi vector<int>
+#define sz size()
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define IO ios_base::sync_with_stdio(0),ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define all(v) ((v).begin()), ((v).end())
+#define rall(v) ((v).rbegin()), ((v).rend())
+
+using namespace std;
+using ll = long long int;
+using ld = long double;
+
+int MOD = 1e9 + 7;
+const double EPS = 1e-9;
+const int oo = INT_MAX;
+const ll OO = 1e18;
+
+const int dx[] = { 1, -1, 0, 0};
+const int dy[] = { 0, 0, 1, -1};
+
+ll GCD(ll a, ll b) { return ((!b) ? a : GCD(b, a%b)); }
+ll LCM(ll a, ll b) { return a / (GCD(a, b))*b; }
+double distance(double x1, double y1, double x2, double y2) { return hypot(fabs(x1 - x2), fabs(y1 - y2)); }
+int comp_double(double a, double b) {
+    if (fabs(a - b) <= EPS) return 0; // equal
+    return (a < b) ? -1 : 1;
+}
+
+const int N = 555;
+int clr[N];
+vector<int> adj[N];
+
+int main() {
+    IO;
+#ifndef ONLINE_JUDGE
+    freopen("In.txt", "r", stdin);
+    freopen("Out.txt","w",stdout);
+#else
+    // freopen("input.in","r",stdin);
+	// freopen("output.out","w",stdout);
+#endif
+    int n, m; cin >> n >> m;
+    for (int i = 0; i < m; ++i) {
+        int u, v; cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    memset(clr, -1, sizeof(clr));
+    for(int i = 1 ; i<= n ; i++){
+        set<int> usedColors;
+        for(auto v : adj[i]){
+            if(~clr[v]){
+                usedColors.insert(clr[v]);
+            }
+        }
+        int color = 1;
+        for(auto i : usedColors){
+            if(i != color) break;
+            color++;
+        }
+        clr[i] = color;
+    }
+
+    for(int i = 1 ; i<=n ; i++){
+        cout << clr[i] << " \n"[i == n];
+    }
+}
