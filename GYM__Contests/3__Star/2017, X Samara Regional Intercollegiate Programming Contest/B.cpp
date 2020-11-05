a69d80bc3895b847064dd359f911319ecc58b222
Abdallah Hemdan <abdallah.ahmed.hemdan@gmail.com>
2020-11-06 00:35:55 +0200

    Create B.cpp

---
+++ GYM__Contests/3__Star/2017, X Samara Regional Intercollegiate Programming Contest/B.cpp
@@ -0,0 +1,78 @@
+// Author : Abdallah Hemdan
+// For Hassanosama, ICPC next year ISA.
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include <bits/stdc++.h>
+
+#define vll vector<ll>
+#define vi vector<int>
+#define sz size()
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define IO ios_base::sync_with_stdio(0),ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define all(v) ((v).begin()), ((v).end())
+#define rall(v) ((v).rbegin()), ((v).rend())
+
+using namespace std;
+using ll = long long int;
+using ld = long double;
+
+int MOD = 1e9 + 7;
+const double EPS = 1e-9;
+const int oo = INT_MAX;
+const ll OO = 1e18;
+
+const int dx[] = {1, -1, 0, 0};
+const int dy[] = {0, 0, 1, -1};
+
+ll GCD(ll a, ll b) { return ((!b) ? a : GCD(b, a % b)); }
+
+ll LCM(ll a, ll b) { return a / (GCD(a, b)) * b; }
+
+double distance(double x1, double y1, double x2, double y2) { return hypot(fabs(x1 - x2), fabs(y1 - y2)); }
+
+int comp_double(double a, double b) {
+    if (fabs(a - b) <= EPS) return 0; // equal
+    return (a < b) ? -1 : 1;
+}
+
+int main() {
+    IO;
+#ifndef ONLINE_JUDGE
+    freopen("In.txt", "r", stdin);
+    freopen("Out.txt", "w", stdout);
+#else
+    // freopen("input.in","r",stdin);
+    // freopen("output.out","w",stdout);
+#endif
+    string s;
+    cin >> s;
+    string p = "happiness";
+    vector<int> ret;
+    for (int i = 0; i < s.size(); i++) {
+        if (s.substr(i, p.size()) == p) {
+            ret.push_back(i);
+        }
+    }
+    if (ret.size() <= 2) {
+        cout << "YES" << endl;
+        if (ret.size() == 0) {
+            swap(s[0], s[1]);
+            if (s.substr(0, p.size()) != p && s.substr(1, p.size()) != p) {
+                cout << "1 2" << endl;
+            } else {
+                cout << "2 3" << endl;
+            }
+        }
+        if (ret.size() == 1) {
+            cout << ret[0] + 1 << ' ' << ret[0] + 2 << endl;
+        }
+        if (ret.size() == 2) {
+            cout << ret[0] + 1 << ' ' << ret[1] + 2 << endl;
+        }
+    } else {
+        cout << "NO" << endl;
+    }
+}
