2c1a024ba52aa6ba9707ace905a81f1419844e9c
Abdallah Hemdan <abdallah.ahmed.hemdan@gmail.com>
2020-11-06 02:43:55 +0200

    Create 7. Linear Diophantine Equation.cpp

---
+++ Math__utilities/7. Linear Diophantine Equation.cpp
@@ -0,0 +1,27 @@
+
+ll eGCD(ll a, ll b, ll &x, ll &y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    ll x1, y1;
+    ll d = eGCD(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+
+// a.x + b.y = c
+bool solveLDE(ll a, ll b, ll c, ll &x0, ll &y0, ll &g) {
+    g = eGCD(abs(a), abs(b), x0, y0);
+    if (c % g) {
+        return false;
+    }
+
+    x0 *= c / g;
+    y0 *= c / g;
+    if (a < 0) x0 = -x0;
+    if (b < 0) y0 = -y0;
+    return true;
+}
