362f32808ac940ff7405bf8bb92e6cff787bf5ab
Abdallah Hemdan <abdallah.ahmed.hemdan@gmail.com>
2020-11-06 02:39:18 +0200

    Create Crucial Equation.cpp

---
+++ SPOJ__Problems/Crucial Equation.cpp
@@ -0,0 +1,94 @@
+// Author : Abdallah Hemdan
+// For Hassanosama, ICPC next year ISA.
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include <bits/stdc++.h>
+
+#define vll vector<ll>
+#define vi vector<int>
+#define sz size()
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define IO ios_base::sync_with_stdio(0),ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define all(v) ((v).begin()), ((v).end())
+#define rall(v) ((v).rbegin()), ((v).rend())
+
+using namespace std;
+using ll = long long int;
+using ld = long double;
+
+int MOD = 1e9 + 7;
+const double EPS = 1e-9;
+const int oo = INT_MAX;
+const ll OO = 1e18;
+
+const int dx[] = {1, -1, 0, 0};
+const int dy[] = {0, 0, 1, -1};
+
+ll GCD(ll a, ll b) { return ((!b) ? a : GCD(b, a % b)); }
+
+ll LCM(ll a, ll b) { return a / (GCD(a, b)) * b; }
+
+double distance(double x1, double y1, double x2, double y2) { return hypot(fabs(x1 - x2), fabs(y1 - y2)); }
+
+int comp_double(double a, double b) {
+    if (fabs(a - b) <= EPS) return 0; // equal
+    return (a < b) ? -1 : 1;
+}
+
+ll eGCD(ll a, ll b, ll &x, ll &y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    ll x1, y1;
+    ll d = eGCD(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+
+// a.x + b.y = c
+bool solveLDE(ll a, ll b, ll c, ll &x0, ll &y0, ll &g) {
+    g = eGCD(abs(a), abs(b), x0, y0);
+    if (c % g) {
+        return false;
+    }
+
+    x0 *= c / g;
+    y0 *= c / g;
+    if (a < 0) x0 = -x0;
+    if (b < 0) y0 = -y0;
+    return true;
+}
+
+int main() {
+    IO;
+#ifndef ONLINE_JUDGE
+    freopen("In.txt", "r", stdin);
+    freopen("Out.txt", "w", stdout);
+#else
+    // freopen("input.in","r",stdin);
+    // freopen("output.out","w",stdout);
+#endif
+    int t;
+    cin >> t;
+    for (int tt = 0; tt < t; tt++) {
+        cout << "Case " << tt + 1 << ": ";
+        ll a, b, c;
+        cin >> a >> b >> c;
+
+        ll x, y, g;
+        g = GCD(abs(a), abs(b));
+
+        bool solutionExists = solveLDE(a, b, c, x, y, g);
+        if (solutionExists) {
+            cout << "Yes" << endl;
+        } else {
+            cout << "No" << endl;
+        }
+    }
+}
