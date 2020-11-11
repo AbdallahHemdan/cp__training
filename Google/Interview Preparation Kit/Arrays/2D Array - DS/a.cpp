2b72f69bd306ff9076abbe478c7a93301ad08028
Abdallah Hemdan <abdallah.ahmed.hemdan@gmail.com>
2020-11-11 16:40:48 +0200

    Update a.cpp

--- Google/Interview Preparation Kit/Arrays/2D Array - DS/a.cpp
+++ Google/Interview Preparation Kit/Arrays/2D Array - DS/a.cpp
@@ -1,16 +1,15 @@
-// Complete the hourglassSum function below.
-int hourglassSum(vector<vector<int>> arr) {
+int hourglassSum(vector<vector<int>> a) {
     int ret = INT_MIN;
     for(int i=0; i< 4; i++) {
         for(int j=0; j<4; j++) {
             int curHour = 0;
             
             for(int k=j; k<j+3;k++) {
-                curHour += arr[i][k];
-                curHour += arr[i + 2][k];
+                curHour += a[i][k];
+                curHour += a[i + 2][k];
             }
 
-            curHour += arr[i+1][j+1];
+            curHour += a[i+1][j+1];
             ret = max(ret, curHour);
         }
     }
