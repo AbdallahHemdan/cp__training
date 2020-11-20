51e29b81a662ba93d928128e1523b59229117040
Abdallah Hemdan <abdallah.ahmed.hemdan@gmail.com>
2020-11-20 16:24:20 +0200

    Update Graph Lib.cpp

--- Graphs__utilities/Graph Lib.cpp
+++ Graphs__utilities/Graph Lib.cpp
@@ -1,10 +1,17 @@
 /**
+* Table of content:
+* 1. Detect cycle in direct graph
+* 2. Detect cycle in un-direct graph
+* */
+
+
+/**
  * Detect if there is a cycle in directed graph
  * hasCycle: bool will be set to true if graph has a cycle
  * */
 int n, m;
-int vis[maxn];
-int color[maxn];
+int vis[N];
+int color[N];
 bool hasCycle = false;
 
 void dfs(int u) {
@@ -21,3 +28,44 @@ void dfs(int u) {
 
   color[u]++;
 }
+
+
+
+
+
+
+
+/**
+ * Detect if there is a cycle in un-directed graph
+ * detectCycle returns bool which represent if there is a cycle or not
+ * */
+int n, m;
+bool vis[N];
+vector<int> adj[N];
+
+bool dfs(int u, int p = -1) {
+  vis[u] = true;
+
+  for (int v : adj[u]) {
+    if (!vis[v]) {
+      if (dfs(v, u)) return true;
+    } else {
+      if (v != p) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+bool detectCycle() {
+  for (int i = 1; i <= n; i++) {
+    if (!vis[i]) {
+      if (dfs(i)) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
