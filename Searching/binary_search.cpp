9238cd4187c7f33b775eca46c7986cedd674f74a
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2021-04-06 06:25:28 +0200

    Add binary search implementation

---
+++ Searching/binary_search.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool ok(int m) {
+  // TODO: to be implemented
+}
+
+/**
+ * The function must be a monotonic function matching the binary search type
+ * (min or max).
+ */
+
+int binarySearch() {
+  int ret;
+  int l = 0, r = 1e9;
+  while (l <= r) {
+    int mid = l + (r - l) / 2;
+
+    if (ok(mid)) {
+    } else {
+    }
+  }
+}
